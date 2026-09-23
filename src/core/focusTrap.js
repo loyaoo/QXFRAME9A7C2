e5555b62f8e5d0ec5a5ca62ac638980b20814624
loyaoo <97509752@qq.com>
2026-09-23 10:05:50 +0800

    security: harden FocusTrap option intake

--- src/core/focusTrap.js
+++ src/core/focusTrap.js
@@ -1,8 +1,9 @@
 
 import { FocusScope } from './focusScope.js';
+import { Utils } from '../utils/utils.js';
 
 function create(options) {
-    var settings = Object.assign({}, options || {}, { mode: 'trap' });
+    var settings = Utils.mergeOwn(options || {}, { mode: 'trap' });
     if (settings.focusOnActivate === undefined) settings.focusOnActivate = true;
     return FocusScope.create(settings);
   }
