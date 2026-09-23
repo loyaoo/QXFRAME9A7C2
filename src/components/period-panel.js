eff8ef33f5978e05dd13a16444dc87e9f6dc70f4
loyaoo <97509752@qq.com>
2026-09-23 09:01:42 +0800

    refactor: use canonical keyboard forwarding

--- src/components/period-panel.js
+++ src/components/period-panel.js
@@ -331,18 +331,7 @@ function create(options) {
       hosted: false,
       disabled: opts.disabled === true,
       navigation: {
-        handlers: {
-          ArrowLeft: function (detail) { return onKeydown(detail.originalEvent); },
-          ArrowRight: function (detail) { return onKeydown(detail.originalEvent); },
-          ArrowUp: function (detail) { return onKeydown(detail.originalEvent); },
-          ArrowDown: function (detail) { return onKeydown(detail.originalEvent); },
-          PageUp: function (detail) { return onKeydown(detail.originalEvent); },
-          PageDown: function (detail) { return onKeydown(detail.originalEvent); },
-          Home: function (detail) { return onKeydown(detail.originalEvent); },
-          End: function (detail) { return onKeydown(detail.originalEvent); },
-          Enter: function (detail) { return onKeydown(detail.originalEvent); },
-          ' ': function (detail) { return onKeydown(detail.originalEvent); }
-        }
+        handlers: KeyboardRegion.forwardHandlers(['ArrowLeft','ArrowRight','ArrowUp','ArrowDown','Home','End','PageUp','PageDown','Enter',' '], onKeydown)
       },
       onEnter: function (detail) {
         var key = activeValue ? DateUnit.key(activeValue, unit, 0) : null;
